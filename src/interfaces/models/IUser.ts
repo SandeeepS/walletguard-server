6319912e8745055d07c9023219f71576cda23441
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-11-19 12:46:33 +0530

    added models, folders for controllers, services , repositories and interfaces

---
+++ src/interfaces/models/IUser.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export interface UserInterface extends Document {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  password: string;
+  email: string;
+  phoneNumber: string;
+  role: string;
+  profilePicture: string;
+  isBlocked: boolean;
+  isDeleted: boolean;
+}
