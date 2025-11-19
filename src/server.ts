06ac709bdab0869517b31a0d29913cfcc7b0c20d
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-11-19 11:42:11 +0530

    connected to mongodb database

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,25 @@ dotenv.config();
 import express from "express";
 import http from "http";
 import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
+
+const uri: string =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/your-app";
+
+(async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 app.get("/", (req, res) => {
   res.status(200).send("Server is running ");
