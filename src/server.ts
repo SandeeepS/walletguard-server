57d7dd646ea8e82a8a77f2652ff27eb56d8f7740
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-11-19 23:48:27 +0530

    added cors

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,48 @@ dotenv.config();
 import express from "express";
 import http from "http";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRoutes";
 const app = express();
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
+
+app.use(
+  cors({
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean | string) => void
+    ) => {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://blog-base-lyart.vercel.app",
+      ];
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+    ],
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+    optionsSuccessStatus: 200,
+  })
+);
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
