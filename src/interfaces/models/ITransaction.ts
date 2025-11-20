71687d7af4586d3f0d26da562d69667f2be8ebce
SandeeepS <sandeepsanthosh3303@gmail.com>
2025-11-20 13:03:31 +0530

    added transaction model and schema

---
+++ src/interfaces/models/ITransaction.ts
@@ -0,0 +1,12 @@
+import type mongoose from "mongoose";
+
+export interface TransactionInterface {
+  _id: mongoose.Types.ObjectId;
+  userId: mongoose.Types.ObjectId;
+  walletId: mongoose.Types.ObjectId;
+  type: string;
+  amount: number;
+  balanceBefore: number;
+  balanceAfter: number;
+  status: string;
+}
---
+++ src/models/transactionModel.ts
@@ -0,0 +1,38 @@
+import mongoose, { Model, Schema } from "mongoose";
+import type { TransactionInterface } from "../interfaces/models/ITransaction";
+
+const transactionSchema: Schema<TransactionInterface> = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  walletId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  balanceBefore: {
+    type: Number,
+    required: true,
+  },
+  balanceAfter: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+const transactionModel: Model<TransactionInterface> =
+  mongoose.model<TransactionInterface>("transactions", transactionSchema);
+
+export default transactionModel;
